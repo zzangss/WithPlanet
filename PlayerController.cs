e105696ff9c8b6e5f16e2f409648bb93480618a6
yerim seo <bmo2683@naver.com>
2025-07-14 23:39:52 +0900

    달리기코드추가

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,19 +4,12 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigid;
     public float movespeed = 5f;
-<<<<<<< HEAD
     public float runmovespeed = 10f;
-=======
->>>>>>> origin/코더서예림
     private Vector2 moveDirection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-<<<<<<< HEAD
         rigid = GetComponent<Rigidbody2D>();
-=======
-        rigid= GetComponent<Rigidbody2D>();
->>>>>>> origin/코더서예림
     }
 
     // Update is called once per frame
@@ -24,20 +17,13 @@ public class PlayerController : MonoBehaviour
     {
         moveDirection.x = Input.GetAxis("Horizontal");
         moveDirection.y = Input.GetAxis("Vertical");
-<<<<<<< HEAD
 
         float currentSpeed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) ? runmovespeed : movespeed;
         moveDirection *= currentSpeed;
-=======
->>>>>>> origin/코더서예림
     }
 
     void FixedUpdate()
     {
-<<<<<<< HEAD
         rigid.MovePosition(rigid.position + moveDirection * Time.fixedDeltaTime);
-=======
-        rigid.MovePosition(rigid.position + moveDirection.normalized * movespeed * Time.fixedDeltaTime);
->>>>>>> origin/코더서예림
     }
 }
