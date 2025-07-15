2024143c238da7d00f737236d4ace1d53b77817f
yerim seo <bmo2683@naver.com>
2025-07-15 15:25:51 +0900

    플레이어 움직임 추가

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,23 +1,30 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
-public class PlayerController : MonoBehaviour
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour
 {
     public Rigidbody2D rigid;
-    public float movespeed = 5f;
-    public float runmovespeed = 10f;
+    public SpriteRenderer spriter;
+    public float movespeed = 4f;
+    public float runmovespeed = 5f;
     private Vector2 moveDirection;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+
     }
 
-   
+    // Update is called once per frame
     void Update()
     {
-        moveDirection.x = Input.GetAxis("Horizontal");
-        moveDirection.y = Input.GetAxis("Vertical");
+        moveDirection.x = Input.GetAxisRaw("Horizontal");
+        moveDirection.y = Input.GetAxisRaw("Vertical");
 
+        moveDirection.Normalize();
 
         float currentSpeed = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift) ? runmovespeed : movespeed;
         moveDirection *= currentSpeed;
@@ -25,6 +32,15 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rigid.MovePosition(rigid.position + moveDirection * Time.fixedDeltaTime);
+        if (moveDirection != Vector2.zero)
+            rigid.MovePosition(rigid.position + moveDirection * Time.fixedDeltaTime);
+    }
+
+    void LateUpdate()
+    {
+        if (moveDirection.x != 0)
+        {
+            spriter.flipX = moveDirection.x < 0;
+        }
     }
 }
