f3ce51316bde60e627323f52f397e51bc0227786
yerim seo <bmo2683@naver.com>
2025-08-13 12:59:45 +0900

    WIP on develop
    세이브시스템 만드는중

---
+++ WithPlanet/Assets/Scripts/UI/SaveSystem/SaveController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveController : MonoBehaviour
+{
+
+    private string saveLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        saveLocation=Path.Combine(Application.persistentDataPath, "savefile.json");
+        LoadGame();
+
+    }
+
+    // Update is called once per frame
+    public void SaveGame()
+    {
+        SaveData saveData = new SaveData()
+        {
+            playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position
+
+        };
+        File.WriteAllText(saveLocation, JsonUtility.ToJson(saveData));
+    }
+
+    public void LoadGame()
+    { 
+        if(File.Exists(saveLocation))
+        {
+            SaveData saveData = JsonUtility.FromJson<SaveData>(File.ReadAllText(saveLocation));
+            GameObject.FindGameObjectWithTag("Player").transform.position = saveData .playerPosition;
+
+        }
+        else
+        {
+            SaveGame();
+        }
+    }
+}
